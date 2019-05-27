add55b8f911d36cf7342b306b078e65a7dac5a1d
vladhuk <vladhookovskiy@gmail.com>
2019-05-28 00:48:08 +0300

    Add properties to chart

--- src/main/java/controller/ChartController.java
+++ src/main/java/controller/ChartController.java
@@ -4,6 +4,8 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.XYChart;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import method.Interpolatable;
 import method.LagrangeInterpolator;
 import method.SplineInterpolator;
@@ -14,15 +16,32 @@ import java.util.function.UnaryOperator;
 
 public class ChartController implements Initializable {
 
-    private static final double START = 0;
-    private static final double END = 10;
-    private static final double INTERVAL = 2;
+    private double start;
+    private double end;
+    private double interval;
 
     @FXML
     private LineChart<Number, Number> lineChart;
 
+    @FXML
+    private TextField startTextField;
+
+    @FXML
+    private TextField endTextField;
+
+    @FXML
+    private TextField intervalTextField;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        drawChart();
+    }
+
+    @FXML
+    void drawChart() {
+        lineChart.getData().clear();
+        readPropertiesFromFields();
+
         UnaryOperator<Double> mainFunction = Math::sin;
 
         Map<Double, Double> controlPoints = getControlPoints(mainFunction);
@@ -38,12 +57,26 @@ public class ChartController implements Initializable {
         drawInterpolation(interpolator, "Lagrange polynomial");
     }
 
+    private void readPropertiesFromFields() {
+        start = getDoubleFromField(startTextField);
+        end = getDoubleFromField(endTextField);
+        interval = getDoubleFromField(intervalTextField);
+    }
+
+    private double getDoubleFromField(TextField field) {
+        try {
+            return Double.parseDouble(field.getText());
+        } catch (NumberFormatException e) {
+            return 0;
+        }
+    }
+
     private void drawMainFunction(UnaryOperator<Double> function, String name) {
         XYChart.Series<Number, Number> series = new XYChart.Series<>();
         series.setName(name);
 
-        double i = START;
-        while (Double.compare(i, END) <= 0) {
+        double i = start;
+        while (Double.compare(i, end) <= 0) {
             series.getData().add(new XYChart.Data<>(i, function.apply(i)));
             i += 0.1;
         }
@@ -54,10 +87,10 @@ public class ChartController implements Initializable {
     private Map<Double, Double> getControlPoints(UnaryOperator<Double> function) {
         Map<Double, Double> points = new LinkedHashMap<>();
 
-        double i = START;
-        while (Double.compare(i, END) <= 0) {
+        double i = start;
+        while (Double.compare(i, end) <= 0) {
             points.put(i, function.apply(i));
-            i += INTERVAL;
+            i += interval;
         }
 
         return points;
@@ -67,8 +100,8 @@ public class ChartController implements Initializable {
         XYChart.Series<Number, Number> series = new XYChart.Series<>();
         series.setName(name);
 
-        double i = START;
-        while (Double.compare(i, END) <= 0) {
+        double i = start;
+        while (Double.compare(i, end) <= 0) {
             series.getData().add(new XYChart.Data<>(i, method.interpolate(i)));
             i += 0.1;
         }
